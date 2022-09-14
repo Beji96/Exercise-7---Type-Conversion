cccb23ea06b2a5da38cfd50be1f480c9d32926c0
Flavius <flaviusbejan96@yahoo.com>
2022-09-14 10:04:10 +0300

    Finish

--- exercise.js
+++ exercise.js
@@ -4,3 +4,6 @@ let isGraduated = false; // string
 console.log(Number(hello));
 console.log(Boolean(age));
 console.log(String(isGraduated));
+/* Javascript does not know that the word Ciao is a
+ representation of the number , so he doesn't know
+ what to do, that why giving me the result of NaN  */
